22d89664a50333ba634d1bebd824b5ddad3883e8
strechyna <brouillasse@gmail.com>
2021-08-16 01:11:34 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
+  if (isCorrectArray(array)) {
+    sort(array);
+    return array[0];
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if (isCorrectArray(array)) {
+    sort(array);
+    return array[array.length - 1];
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (isCorrectArray(array)) {
+    return sum(array) / array.length;
+  }
   return 0;
+}
+
+function isCorrectArray (array) {
+  return array != null && array != undefined &&  array.length > 0;
+}
+
+function sort (array) {
+  array.sort((el1, el2) => el1 - el2);
+}
+
+function sum (array) {
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+
+  return sum;
 }
